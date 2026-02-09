35b496958cbfc1ec64ddc8b1ff03ee157807f3c7
Aris <64918822+Arisamiga@users.noreply.github.com>
2026-02-09 22:13:41 +0000

    Initial tailwindcss integration

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div
+      class="wrapper flex flex-col items-center text-center bg-red-600 text-white p-8 rounded-lg shadow-lg mt-8"
+    >
+      <h1 class="text-4xl font-bold mb-2">Tech Week</h1>
+      <p class="text-lg mb-0">Dublin City University 15 - 29 March 2017</p>
     </div>
   </header>
 
