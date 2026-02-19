d0e6f62ac9640dae70dd5eacc7d55114460b39e3
Aris <64918822+Arisamiga@users.noreply.github.com>
2026-02-19 13:00:11 +0000

    Made CFT more general

--- src/pages/CallForTalks.vue
+++ src/pages/CallForTalks.vue
@@ -21,22 +21,7 @@ import redbrickLogo from "../assets/redbrick.svg"
           for people to come and talk to our members. We'd like to hear from you! Talks we've had in
           the past can be found at
           <a href="https://techweek.dcu.ie" class="text-blue-600 underline">techweek.dcu.ie</a>.
-          This year, we are looking for talks that fit the following themes:
         </p>
-        <ul class="list-disc pl-6 mb-6 text-gray-700 dark:text-gray-200">
-          <li>
-            <span class="font-medium">Security</span>
-            <span class="text-sm text-gray-500 dark:text-gray-400">(15/03/17)</span>
-          </li>
-          <li>
-            <span class="font-medium">Development tools</span>
-            <span class="text-sm text-gray-500 dark:text-gray-400">(22/03/2017)</span>
-          </li>
-          <li>
-            <span class="font-medium">Mobile Development</span>
-            <span class="text-sm text-gray-500 dark:text-gray-400">(29/03/2017)</span>
-          </li>
-        </ul>
         <h3 class="text-xl font-semibold mt-6 mb-2 text-blue-600 dark:text-blue-300">
           What do we want?
         </h3>
@@ -44,11 +29,8 @@ import redbrickLogo from "../assets/redbrick.svg"
           We want people to come and talk to us! We'd love to hear about any relevant topic you're
           passionate about, or want to spread the word about. We're open to any ideas—if you have a
           talk that doesn't fit these themes but you want to give it anyway, let us know and we will
-          organise something with you. Because the format of Techweek is a bit different this year,
-          we are looking for speakers to come in on
-          <span class="font-medium">Wednesday the 15th, 22nd, and 29th</span> at
-          <span class="font-medium">3, 4, or 5pm</span>. If you think you have an idea for a talk,
-          workshop, story, or demo, just email us at
+          organise something with you. If you think you have an idea for a talk, workshop, story, or
+          demo, just email us at
           <a href="mailto:committee@redbrick.dcu.ie" class="text-blue-600 underline font-medium"
             >committee@redbrick.dcu.ie</a
           >
